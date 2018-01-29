9ae13cff9699bd1b95e41086f4fc37c3a3ac55a5
Kennedy Alves <alvkennedy@gmail.com>
2018-01-29 12:29:09 -0200

    Adicionado mais uma condição na validação

--- bash/check-container.sh
+++ bash/check-container.sh
@@ -2,7 +2,7 @@
 
 docker_home='/etc/docker/docker-config/docker'
 container=$(docker ps | grep "docker-registry.local" | awk -F '/' '{print $2}' | cut -d " " -f1)
-  if [[ -z "$container" ]]; then
+  if [ -z "$container" ] || [ $(cat "$container" | wc -l) -lt '2' ]; then
     echo "Container Parado"
     echo "Iniciando os containers"
     $( cd "$docker_home" && /usr/local/bin/docker-compose up -d)
